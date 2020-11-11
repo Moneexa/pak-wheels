7e6eae8e3c2769917a646f85a2f5c6c78af413ca
Moneexa <14besemsyed@seecs.edu.pk>
2020-11-11 21:23:11 +0500

    replaced nav with material-ui

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -9,6 +9,21 @@ import { Nav } from 'react-bootstrap';
 import './Layout.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu"
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
 
 export default function Layout() {
     const [sidebarOpened, setSidebarOpened] = useState(false);
@@ -16,97 +31,124 @@ export default function Layout() {
     function toggleSidebar() {
         setSidebarOpened(!sidebarOpened);
     }
-    return (<div className="dashboard">
-        <div style={{
-            padding: "20px 15px 15px",
-            position: "relative", background: "#233d7b",
-            marginBottom: "10px",
-            boxShadow: "0 0 10px 0 rgba(35,61,123,.8)"
-        }}>
-            <div className="d-flex flex-column">
-                <div className="d-flex" style={{ justifyContent: "space-between" }}>
-                    <button className="float-left height-20" type="button" onClick={() => toggleSidebar()}>
-                        {sidebarOpened ? "Close" : "Open"}
-                    </button>
-                    <div>
-                        <img className="d-flex align-items-center justify-content-center" height="40"
-                            src="https://lite1.pakwheels.com/app/6.0.12/assets/images/PakWheels-Tagline-Logo.svg" />
+    return (<>
+        <div className="dashboard">
+            <div style={{
+                padding: "20px 15px 15px",
+                position: "relative", background: "#233d7b",
+                marginBottom: "10px",
+                boxShadow: "0 0 10px 0 rgba(35,61,123,.8)"
+            }}>
+                <div className="d-flex flex-column">
+                    <div className="d-flex" style={{ justifyContent: "space-between" }}>
+                        <IconButton
+                            color="default"
+                            aria-label="open drawer"
+                            edge="start"
+                            onClick={() => toggleSidebar()}
+                            className="float-left height-20"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <div>
+                            <img className="d-flex align-items-center justify-content-center" height="40"
+                                src="https://lite1.pakwheels.com/app/6.0.12/assets/images/PakWheels-Tagline-Logo.svg" />
+                        </div>
+                        <div className="d-flex align-items-center justify-content-center push-right">
+                            <FontAwesomeIcon icon={faHeart} style={{ color: "white", verticalAlign: "middle" }} />
+                        </div>
                     </div>
-                    <div className="d-flex align-items-center justify-content-center push-right">
-                        <FontAwesomeIcon icon={faHeart} style={{ color: "white", verticalAlign: "middle" }} />
-                    </div>
-                </div>
-                <div className="d-flex align-items-center justify-content-center search-area my-3">
-                    <button className="btn btn-block btn-white"
-                        style={{
-                            padding: "10px 16px 12px",
-                            fontSize: "15px",
-                            textAlign: "left",
-                            fontFamily: "font-custom-icons,Lato,sans-serif",
-                            whiteSpace: "pre-wrap",
-                            borderRadius: "25px",
-                            border: "none",
-                            width: "100%",
-                            display: "block",
-                            backgroundColor: "white"
+                    <div className="d-flex align-items-center justify-content-center search-area my-3">
+                        <button className="btn btn-block btn-white"
+                            style={{
+                                padding: "10px 16px 12px",
+                                fontSize: "15px",
+                                textAlign: "left",
+                                fontFamily: "font-custom-icons,Lato,sans-serif",
+                                whiteSpace: "pre-wrap",
+                                borderRadius: "25px",
+                                border: "none",
+                                width: "100%",
+                                display: "block",
+                                backgroundColor: "white"
 
-                        }}
-       >Start searching new cars</button>
+                            }}
+                        >Start searching new cars</button>
+                    </div>
                 </div>
             </div>
-        </div>
-        {
-            sidebarOpened ?
-                <div id="wrapper" style={{
-                    minWidth: "280px",
-                    color: "#373737",
-                    backgroundColor: "#fff",
-                    top: "0", position: "absolute"
+            {
+                sidebarOpened ?
+                    <div id="wrapper"
+                        style={{
+                            minWidth: "280px",
+                            color: "#373737",
+                            backgroundColor: "#fff",
+                            top: "0", position: "absolute"
 
-                }}>
-                    <div className="d-flex flex-column">
+                        }}>
+
                         <Router>
-                            <div className="sidebar-wheel d-flex align-items-center justify-content-center">
-                                <Link to="/"  style={{ marginTop: "20px", marginBottom: "30px", borderBottom:"1px solid #e6e6e6" }}>
-                                    <img src="https://lite1.pakwheels.com/app/6.0.12/assets/images/PakWheels-Blue-Tagline-Logo.svg" height="50" />
-                                </Link>
+                            <nav className="d-flex flex-column">
+                                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                                <Hidden smUp implementation="css">
+                                    <Drawer
 
-                            </div>
-                            <Nav className="d-flex flex-column">
-                                <Nav.Item style={{ marginBottom:"2.5rem" ,borderBottom:"1px solid #e6e6e6"}}>
-                                    <Nav.Link>
+                                        variant="temporary"
+                                        //anchor={theme.direction === "rtl" ? "right" : "left"}
+                                        open={sidebarOpened}
+                                        onClose={toggleSidebar}
+                                        // classes={{
+                                        //     paper: classes.drawerPaper
+                                        // }}
+                                        ModalProps={{
+                                            keepMounted: true // Better open performance on mobile.
+                                        }}
+                                    >
+                                        <div>
+                                            <Divider />
+                                            <List>
+                                                <ListItem key="home" component={Link} to="/">
+                                                    <div className="sidebar-wheel d-flex align-items-center justify-content-center">
+                                                        <Link to="/" style={{ marginTop: "20px", marginBottom: "30px", borderBottom: "1px solid #e6e6e6" }}>
+                                                            <img src="https://lite1.pakwheels.com/app/6.0.12/assets/images/PakWheels-Blue-Tagline-Logo.svg" height="50" />
+                                                        </Link>
 
+                                                    </div>
+                                                </ListItem>
+                                                <ListItem key="Used Cars" component={Link} to="/"
+                                                    style={{ marginBottom: "2.5rem", borderBottom: "1px solid #e6e6e6" }}>
+                                                    <ListItemIcon>
+                                                    </ListItemIcon>
+                                                    <ListItemText primary="Used Cars" />
+                                                </ListItem>
+                                                <ListItem key="Buy New Cars" component={Link} to="/"
+                                                    style={{ marginBottom: "2.5rem", borderBottom: "1px solid #e6e6e6" }}
 
-                                        <span>Used Cars</span>
+                                                >
+                                                    <ListItemIcon>
+                                                    </ListItemIcon>
+                                                    <ListItemText primary="Buy New Cars" />
+                                                </ListItem>
+                                                <ListItem key="Post Adds" component={Link} to="/"
+                                                    style={{ marginBottom: "2.5rem", borderBottom: "1px solid #e6e6e6" }}
+                                                >
 
+                                                    <ListItemIcon>
+                                                    </ListItemIcon>
+                                                    <ListItemText primary="Post Adds" />
+                                                </ListItem>
 
-                                    </Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item style={{ marginBottom:"2.5rem" , borderBottom:"1px solid #e6e6e6"}}>
-                                    <Nav.Link>
-
-
-                                        <span>New  Cars</span>
-
-                                    </Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item style={{ marginBottom:"2.5rem" , borderBottom:"1px solid #e6e6e6"}}>
-                                    <Nav.Link>
-
-
-                                        <span>Bikes</span>
-
-                                    </Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link></Nav.Link>
-                                </Nav.Item>
-                            </Nav>
+                                            </List>
+                                        </div>
+                                    </Drawer>
+                                </Hidden>
+                            </nav>
                         </Router>
                     </div>
-                </div >
-                : ''
-        }
 
-    </div >)
+                    : ''
+            }
+
+        </div ></>)
 }
