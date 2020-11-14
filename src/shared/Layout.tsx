39cdeca31cb46df083a420ffd98de517c4cefc20
Moneeza Syed <14besemsyed@seecs.edu.pk>
2020-11-14 19:34:37 +0500

    added links

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -20,17 +20,29 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+import Button from '@material-ui/core/Button'
 
 
 export default function Layout() {
     const [sidebarOpened, setSidebarOpened] = useState(false);
+    const [expanded, setExpanded] = useState(false);
 
     function toggleSidebar() {
         setSidebarOpened(!sidebarOpened);
     }
+    const handleChange = (panel: boolean) => (isExpanded: boolean) => {
+        setExpanded(isExpanded ? panel : false);
+    };
     return (<>
         <div className="dashboard">
             <div style={{
@@ -42,7 +54,7 @@ export default function Layout() {
                 <div className="d-flex flex-column">
                     <div className="d-flex" style={{ justifyContent: "space-between" }}>
                         <IconButton
-                            color="default"
+                            style={{ color: "white" }}
                             aria-label="open drawer"
                             edge="start"
                             onClick={() => toggleSidebar()}
@@ -107,39 +119,63 @@ export default function Layout() {
                                     >
                                         <div>
                                             <Divider />
-                                            <List>
-                                                <ListItem key="home" component={Link} to="/">
-                                                    <div className="sidebar-wheel d-flex align-items-center justify-content-center">
-                                                        <Link to="/" style={{ marginTop: "20px", marginBottom: "30px", borderBottom: "1px solid #e6e6e6" }}>
-                                                            <img src="https://lite1.pakwheels.com/app/6.0.12/assets/images/PakWheels-Blue-Tagline-Logo.svg" height="50" />
-                                                        </Link>
 
-                                                    </div>
-                                                </ListItem>
-                                                <ListItem key="Used Cars" component={Link} to="/"
-                                                    style={{ marginBottom: "2.5rem", borderBottom: "1px solid #e6e6e6" }}>
-                                                    <ListItemIcon>
-                                                    </ListItemIcon>
-                                                    <ListItemText primary="Used Cars" />
-                                                </ListItem>
-                                                <ListItem key="Buy New Cars" component={Link} to="/"
-                                                    style={{ marginBottom: "2.5rem", borderBottom: "1px solid #e6e6e6" }}
+                                            <div className="sidebar-wheel d-flex align-items-center justify-content-center">
+                                                <Link to="/" style={{ marginTop: "20px", marginBottom: "30px", borderBottom: "1px solid #e6e6e6" }}>
+                                                    <img src="https://lite1.pakwheels.com/app/6.0.12/assets/images/PakWheels-Blue-Tagline-Logo.svg" height="50" />
+                                                </Link>
 
+                                            </div>
+                                            <Accordion>
+                                                <AccordionSummary
+                                                    expandIcon={<ExpandMoreIcon />}
+                                                    aria-controls="panel1a-content"
+                                                    id="panel1a-header"
                                                 >
-                                                    <ListItemIcon>
-                                                    </ListItemIcon>
-                                                    <ListItemText primary="Buy New Cars" />
-                                                </ListItem>
-                                                <ListItem key="Post Adds" component={Link} to="/"
-                                                    style={{ marginBottom: "2.5rem", borderBottom: "1px solid #e6e6e6" }}
+                                                    <Typography>Post Add</Typography>
+                                                </AccordionSummary>
+                                                <AccordionDetails>
+                                                    <Typography>
+                                                        <ul style={{ listStyle: "none" }}>
+                                                            <li>
+                                                              <Link to="/used-cars">  buy cars </Link>
+                                                            </li>
+                                                            <li>
+                                                                new cars
+                                                            </li>
+                                                        </ul>
+                                                    </Typography>
+                                                </AccordionDetails>
+                                            </Accordion>
+                                            <Accordion style={{ paddingTop: "30px", paddingBottom: "20px" }}>
+                                                <AccordionSummary
+                                                    expandIcon={<ExpandMoreIcon />}
+                                                    aria-controls="panel1a-content"
+                                                    id="panel1a-header"
                                                 >
+                                                    <Typography>Post Add</Typography>
+                                                </AccordionSummary>
+                                                <AccordionDetails>
+                                                    <Typography>
+                                                        <ul style={{ listStyle: "none" }}>
+                                                            <li>
+                                                                buy cars
+                                                            </li>
+                                                            <li>
+                                                                new cars
+                                                            </li>
+                                                        </ul>
+                                                    </Typography>
+                                                </AccordionDetails>
+                                            </Accordion>
+                                            <ListItem key="Post Adds" component={Link} to="/"
+                                                style={{ marginBottom: "2.5rem", borderBottom: "1px solid #e6e6e6" }}
+                                            >
 
-                                                    <ListItemIcon>
-                                                    </ListItemIcon>
-                                                    <ListItemText primary="Post Adds" />
-                                                </ListItem>
-
-                                            </List>
+                                                <ListItemIcon>
+                                                </ListItemIcon>
+                                                <ListItemText primary="Post Adds" />
+                                            </ListItem>
                                         </div>
                                     </Drawer>
                                 </Hidden>
@@ -149,6 +185,15 @@ export default function Layout() {
 
                     : ''
             }
+            <div className="d-flex align-items-center justify-content-center overflow-auto">
+                <Card style={{ width: "100%" }}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            <b>Browse Used Cars</b>
+                        </Typography>
 
+                    </CardContent>
+                </Card>
+            </div>
         </div ></>)
 }
