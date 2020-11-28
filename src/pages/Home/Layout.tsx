aadccb3f477c030ebb7d1b2e68c11541627ed496
Moneeza Syed <14besemsyed@seecs.edu.pk>
2020-11-28 14:36:46 +0500

    added material-ui

--- src/pages/Home/Layout.tsx
+++ src/pages/Home/Layout.tsx
@@ -6,6 +6,11 @@ import {
     Switch,
 } from "react-router-dom";
 import { Nav } from 'react-bootstrap';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+
 import './Layout.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
@@ -25,24 +30,107 @@ import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions'
 import Button from '@material-ui/core/Button'
+const AntTabs = withStyles({
+    root: {
+        borderBottom: '1px solid #e8e8e8',
+    },
+    indicator: {
+        backgroundColor: '#1890ff',
+    },
+})(Tabs);
 
+const AntTab = withStyles((theme) => ({
+    root: {
+        textTransform: 'none',
+        minWidth: 72,
+        fontWeight: theme.typography.fontWeightRegular,
+        marginRight: theme.spacing(4),
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+        '&:hover': {
+            color: '#40a9ff',
+            opacity: 1,
+        },
+        '&$selected': {
+            color: '#1890ff',
+            fontWeight: theme.typography.fontWeightMedium,
+        },
+        '&:focus': {
+            color: '#40a9ff',
+        },
+    },
+    selected: {},
+}))((props: { label: string }) => <Tab disableRipple {...props} />);
+
+const StyledTabs = withStyles({
+    indicator: {
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundColor: 'transparent',
+        '& > span': {
+            maxWidth: 40,
+            width: '100%',
+            backgroundColor: '#635ee7',
+        },
+    },
+})((props:{value:number,onChange:(event:any) => void}) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
+
+const StyledTab = withStyles((theme) => ({
+    root: {
+        textTransform: 'none',
+        color: '#fff',
+        fontWeight: theme.typography.fontWeightRegular,
+        fontSize: theme.typography.pxToRem(15),
+        marginRight: theme.spacing(1),
+        '&:focus': {
+            opacity: 1,
+        },
+    },
+}))((props:{label:string}) => <Tab disableRipple {...props} />);
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    padding: {
+        padding: theme.spacing(3),
+    },
+    demo1: {
+        backgroundColor: theme.palette.background.paper,
+    },
+    demo2: {
+        backgroundColor: '#2e1534',
+    },
+}));
 
 export default function Layout() {
     const [sidebarOpened, setSidebarOpened] = useState(false);
-    const [expanded, setExpanded] = useState(false);
+    const classes = useStyles();
+    const [value, setValue] = useState<number>(0);
+
+    const handleChange = (event: any) => {
+        setValue(event.target.value);
+    };
 
     function toggleSidebar() {
         setSidebarOpened(!sidebarOpened);
     }
-    const handleChange = (panel: boolean) => (isExpanded: boolean) => {
-        setExpanded(isExpanded ? panel : false);
-    };
+
+
     return (<>
         <div className="dashboard">
             <div style={{
@@ -138,7 +226,7 @@ export default function Layout() {
                                                     <Typography>
                                                         <ul style={{ listStyle: "none" }}>
                                                             <li>
-                                                              <Link to="/used-cars">  buy cars </Link>
+                                                                <Link to="/used-cars">  buy cars </Link>
                                                             </li>
                                                             <li>
                                                                 new cars
@@ -191,7 +279,26 @@ export default function Layout() {
                         <Typography color="textSecondary" gutterBottom>
                             <b>Browse Used Cars</b>
                         </Typography>
-
+                        <div className={classes.root}>
+                            <div className={classes.demo1}>
+                                <AntTabs value={value} onChange={handleChange} aria-label="ant example">
+                                    <AntTab label="Tab 1" />
+                                    <AntTab label="Tab 2" />
+                                    <AntTab label="Tab 3" />
+                                </AntTabs>
+                                <Typography className={classes.padding} />
+                            </div>
+                            <div className={classes.demo2}>
+                                <StyledTabs value={value} 
+                                
+                                onChange={handleChange} aria-label="styled tabs example">
+                                    <StyledTab label="Workflows" />
+                                    <StyledTab label="Datasets" />
+                                    <StyledTab label="Connections" />
+                                </StyledTabs>
+                                <Typography className={classes.padding} />
+                            </div>
+                        </div>
                     </CardContent>
                 </Card>
             </div>
